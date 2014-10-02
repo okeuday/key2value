bf4f86410db083bfbd23e99af35a70e7fc21f318
Michael Truog <mjtruog@gmail.com>
2014-10-02 10:57:29 -0700

    Add hex.pm package.

---
+++ package.exs
@@ -0,0 +1,25 @@
+defmodule Key2value.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :key2value,
+     version: "1.3.3",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+  end
+
+  defp description do
+    "Erlang 2-way Set Associative Map"
+  end
+
+  defp package do
+    [files: ~w(src doc README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"GitHub" => "https://github.com/okeuday/key2value"}]
+   end
+end
