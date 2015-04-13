528c074fd2bab4b885c0cc58af23e064e1014728
Michael Truog <mjtruog@gmail.com>
2015-04-12 17:08:45 -0700

    Version 1.5.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Key2value.Mixfile do
 
   def project do
     [app: :key2value,
-     version: "1.4.0",
+     version: "1.5.0",
      language: :erlang,
      description: description,
      package: package,
