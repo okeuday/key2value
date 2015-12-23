f7767374129ca1771241569a235ec8d715470c42
Michael Truog <mjtruog@gmail.com>
2015-12-23 12:50:38 -0800

    Version 1.5.1.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Key2value.Mixfile do
 
   def project do
     [app: :key2value,
-     version: "1.5.0",
+     version: "1.5.1",
      language: :erlang,
      description: description,
      package: package,
@@ -20,7 +20,7 @@ defmodule Key2value.Mixfile do
 
   defp package do
     [files: ~w(src doc rebar.config README.markdown),
-     contributors: ["Michael Truog"],
+     maintainers: ["Michael Truog"],
      licenses: ["BSD"],
      links: %{"GitHub" => "https://github.com/okeuday/key2value"}]
    end
