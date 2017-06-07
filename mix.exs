9ea68362f3693e5cd0159ffccfb140a76057629e
Michael Truog <mjtruog@gmail.com>
2017-06-07 09:41:56 -0700

    Version 1.7.1.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Key2value.Mixfile do
 
   def project do
     [app: :key2value,
-     version: "1.7.0",
+     version: "1.7.1",
      language: :erlang,
      description: description(),
      package: package(),
