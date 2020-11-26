bee9b334223ac11d573de17bf7be029d83c09739
Michael Truog <mjtruog@protonmail.com>
2020-11-26 14:50:09 -0800

    Version 2.0.1.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Key2value.Mixfile do
 
   def project do
     [app: :key2value,
-     version: "2.0.0",
+     version: "2.0.1",
      language: :erlang,
      erlc_options: [
        {:d, :erlang.list_to_atom('ERLANG_OTP_VERSION_' ++ :erlang.system_info(:otp_release))},
@@ -38,7 +38,7 @@ defmodule Key2value.Mixfile do
   end
 
   defp package do
-    [files: ~w(src doc rebar.config README.markdown),
+    [files: ~w(src doc rebar.config README.markdown LICENSE),
      maintainers: ["Michael Truog"],
      licenses: ["MIT"],
      links: %{"GitHub" => "https://github.com/okeuday/key2value"}]
